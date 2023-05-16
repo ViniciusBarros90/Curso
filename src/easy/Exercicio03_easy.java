235ee46a3b0a3928e3ecad5b5f2d97de78f17fea
ViniciusBarros90 <133717360+ViniciusBarros90@users.noreply.github.com>
2023-05-16 15:53:45 -0300

    Primeiro Commit

---
+++ src/easy/Exercicio03_easy.java
@@ -0,0 +1,22 @@
+package easy;
+
+import javax.swing.JOptionPane;
+
+public class Exercicio03_easy {
+    
+            static String primeiroValor; //varialvel
+            static String segundoValor; //variavel 
+
+            public static void main(String[] args) { 
+                primeiroValor = JOptionPane.showInputDialog("Digite o primeiro valor");
+                segundoValor = JOptionPane.showInputDialog("Digite o segundo valor");
+            
+            System.out.println("Primeiro Valor digitado" +segundoValor);
+            System.out.println("Segundo Valor digitado"  +primeiroValor);
+            }
+
+            
+
+    
+    
+}
