97c9198beaa85e0f60feb392a64d3d3f5f09e087
ViniciusBarros90 <vinicius_santoss@hotmail.com>
2023-05-17 14:40:18 -0300

    Refatoração Exercicio 04 Easy

--- src/easy/Execucao.java
+++ src/easy/Execucao.java
@@ -2,19 +2,29 @@ package easy;
 
 import javax.swing.JOptionPane;
 
+
 public class Execucao {
     public static void main(String[] args) {
         
         //Exercicio02_easy
-        Exercicio02_easy exercicio02_easy = new Exercicio02_easy(); // instanciando a classe Exercicio02_easy
-        String palavra = exercicio02_easy.retornaPalavra("Vinicius Barros");// fazendo uma chamada e salvando em uma variável
-        System.out.println(palavra); // impressão no console
+        //Exercicio02_easy exercicio02_easy = new Exercicio02_easy(); // instanciando a classe Exercicio02_easy
+        //String palavra = exercicio02_easy.retornaPalavra("Vinicius Barros");// fazendo uma chamada e salvando em uma variável
+        //System.out.println(palavra); // impressão no console
 
         //Exercicio03_easy
-        String primeiroValor = JOptionPane.showInputDialog("Digite o primeiro valor");
-        String segundoValor =  JOptionPane.showInputDialog("Digite o segundo valor");
-        Exercicio03_easy exercicio03_easy = new Exercicio03_easy(); // instanciando a classe Exercicio03_easy
-        System.out.println(exercicio03_easy.retornaValorTrocado(primeiroValor, segundoValor)); //Faço a chamada e exibo o valor
+       // String primeiroValor = JOptionPane.showInputDialog("Digite o primeiro valor");
+        //String segundoValor =  JOptionPane.showInputDialog("Digite o segundo valor");
+        //Exercicio03_easy exercicio03_easy = new Exercicio03_easy(); // instanciando a classe Exercicio03_easy
+        //System.out.println(exercicio03_easy.retornaValorTrocado(primeiroValor, segundoValor)); //Faço a chamada e exibo o valor
+    
+        //Exercicio04_easy
+        String numeroDigitadoUsuario = JOptionPane.showInputDialog("Digite um numero");
+        int numeroDigitado = Integer.parseInt(numeroDigitadoUsuario);
+        Exercicio04_easy exercicio04_easy = new Exercicio04_easy();
+        System.out.println("O dobro de " + numeroDigitado +" é " +exercicio04_easy.calcularDobro(numeroDigitado));
+                
+        
+
     }
 
 }
---
+++ src/easy/Exercicio04_easy.java
@@ -0,0 +1,12 @@
+package easy;
+
+public class Exercicio04_easy {
+
+    public int calcularDobro(int numeroDigitado) {
+        int resultado;
+        resultado = numeroDigitado * 2;
+        return resultado;
+     
+    }
+
+}
