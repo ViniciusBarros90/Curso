35c2559cf8a1f767e554be2f3cf1536b41b8bbcb
ViniciusBarros90 <vinicius_santoss@hotmail.com>
2023-05-16 21:23:20 -0300

    Refatoração Exercicio 03 Easy

--- src/easy/Execucao.java
+++ src/easy/Execucao.java
@@ -1,10 +1,20 @@
 package easy;
 
+import javax.swing.JOptionPane;
+
 public class Execucao {
     public static void main(String[] args) {
-        Exercicio02_easy exercicio02_easy = new Exercicio02_easy();
-        String palavra = exercicio02_easy.retornaPalavra("Vinicius Barros");
-        System.out.println(palavra);
+        
+        //Exercicio02_easy
+        Exercicio02_easy exercicio02_easy = new Exercicio02_easy(); // instanciando a classe Exercicio02_easy
+        String palavra = exercicio02_easy.retornaPalavra("Vinicius Barros");// fazendo uma chamada e salvando em uma variável
+        System.out.println(palavra); // impressão no console
+
+        //Exercicio03_easy
+        String primeiroValor = JOptionPane.showInputDialog("Digite o primeiro valor");
+        String segundoValor =  JOptionPane.showInputDialog("Digite o segundo valor");
+        Exercicio03_easy exercicio03_easy = new Exercicio03_easy(); // instanciando a classe Exercicio03_easy
+        System.out.println(exercicio03_easy.retornaValorTrocado(primeiroValor, segundoValor)); //Faço a chamada e exibo o valor
     }
-    
-} 
+
+}
--- src/easy/Exercicio03_easy.java
+++ src/easy/Exercicio03_easy.java
@@ -3,20 +3,14 @@ package easy;
 import javax.swing.JOptionPane;
 
 public class Exercicio03_easy {
-    
-            static String primeiroValor; //varialvel
-            static String segundoValor; //variavel 
 
-            public static void main(String[] args) { 
-                primeiroValor = JOptionPane.showInputDialog("Digite o primeiro valor");
-                segundoValor = JOptionPane.showInputDialog("Digite o segundo valor");
-            
-            System.out.println("Primeiro Valor digitado" +segundoValor);
-            System.out.println("Segundo Valor digitado"  +primeiroValor);
-            }
+    static String primeiroValor; // varialvel
+    static String segundoValor; // variavel
 
-            
+    public String retornaValorTrocado(String primeiroValor, String segundoValor) {
 
-    
-    
+        return "Primeiro Valor digitado" +  segundoValor + "\n" +
+                "Segundo Valor digitado" +  primeiroValor;
+    }
+
 }
